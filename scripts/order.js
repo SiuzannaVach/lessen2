c2b88fdda38a0304b99ffab1e30c0167093ebaca
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-09-08 14:03:49 +0200

    Fix: update total price calculation logic in Order.js

--- scripts/order.js
+++ scripts/order.js
@@ -22,6 +22,9 @@ const orderIdValue = document.getElementById("orderId");
 
 // Элементы карточек размеров
 const sizes = document.querySelectorAll(".main-size-card");
+
+// Элементы карточек скоростей
+const speeds = document.querySelectorAll(".main-speed-card");
 // Переменные для карты, маршрута и расчетов
 let map;
 let mapRoute;
@@ -45,16 +48,18 @@ ymaps.ready(() => {
   new ymaps.SuggestView("from");
   new ymaps.SuggestView("to");
 
-  // Логика выбора размера посылки
-  sizes.forEach((element) => {
-    element.addEventListener("click", () => {
-      sizes.forEach((c) =>
-        c.classList.toggle(
-          "is-active",
-          c.dataset.value === element.dataset.value,
-        ),
-      );
-      renderInfo();
+  // Логика выбора размера посылки и скорости доставки
+  [sizes, speeds].forEach((group) => {
+    group.forEach((element) => {
+      element.addEventListener("click", () => {
+        group.forEach((c) =>
+          c.classList.toggle(
+            "is-active",
+            c.dataset.value === element.dataset.value,
+          ),
+        );
+        renderInfo();
+      });
     });
   });
   // Дизейблим кнопку Рассчитать если одного или двух значений нет
@@ -102,6 +107,14 @@ calcButton.addEventListener("click", () => {
       // Просчитываем длительность доставки
       let duration = Math.min(30, 1 + Math.ceil(km / 80));
 
+      // Увеличиваем на 15% и сокращаем время на 30%
+      const speed = document.querySelector(".main-speed-card.is-active").dataset
+        .value;
+      if (speed === "fast") {
+        total = Math.ceil(total * 1.15);
+        duration = Math.ceil(duration - duration * 0.3);
+      }
+
       calculation = {
         from: fromInput.value,
         to: toInput.value,
@@ -110,6 +123,7 @@ calcButton.addEventListener("click", () => {
         duration: duration,
         rate: RATES[size],
         total: total,
+        speed: speed,
       };
 
       // Выводим результат на экран.
